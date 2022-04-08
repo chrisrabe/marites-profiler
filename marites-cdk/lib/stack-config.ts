713384c3283b8c9bf0e9260d7686e06f06b30ae8
Chris Rabe <chris.rabe14@gmail.com>
2022-04-09 09:03:14 +1000

    fix: fixed error with IDs

--- marites-cdk/lib/stack-config.ts
+++ marites-cdk/lib/stack-config.ts
@@ -3,7 +3,7 @@ import { Duration } from "aws-cdk-lib";
 const config = {
   // S3 configuration
   inputBucketName: "marites-comprehend-input",
-  outputBucketName: "marites-comprehend-input",
+  outputBucketName: "marites-comprehend-output",
   expiryDuration: Duration.hours(5),
   // IAM configuration
   initialUsers: ["marites-user"],
