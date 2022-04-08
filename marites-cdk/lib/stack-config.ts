3fec49fae73b04fa517ca90416cfa717e646231b
Chris Rabe <chris.rabe14@gmail.com>
2022-04-09 09:06:05 +1000

    fix: fixed duration

--- marites-cdk/lib/stack-config.ts
+++ marites-cdk/lib/stack-config.ts
@@ -4,7 +4,7 @@ const config = {
   // S3 configuration
   inputBucketName: "marites-comprehend-input",
   outputBucketName: "marites-comprehend-output",
-  expiryDuration: Duration.hours(5),
+  expiryDuration: Duration.days(1), // has to be days
   // IAM configuration
   initialUsers: ["marites-user"],
   minUserPolicies: ["ComprehendFullAccess"],
