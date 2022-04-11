357f54262a37248fd07df9e7b27b441c7fd69d78
Chris Rabe <chris.rabe14@gmail.com>
2022-04-11 18:04:59 +1000

    feat: set up tailwindCSS

---
+++ marites-ui/src/modules/GeneralNews/GeneralNews.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GeneralNews: React.FC = () => {
+  return (
+    <div>
+      <p className="text-lg">Hello world</p>
+    </div>
+  );
+};
+
+export default GeneralNews;
