971a9f6f26992ad5e90ad1054eecf2f0f2611a6c
Chris Rabe <chris.rabe14@gmail.com>
2022-04-11 23:12:20 +1000

    feat: made news articles more responsive

--- marites-ui/src/components/common/NewsArticle/NewsArticle.tsx
+++ marites-ui/src/components/common/NewsArticle/NewsArticle.tsx
@@ -15,7 +15,7 @@ const NewsArticle: React.FC<NewsArticleProps> = ({ imageUrl, title, date }) => {
       whileTap={{ scale: 0.9 }}
       className="cursor-pointer"
     >
-      <div className="object-cover w-96 h-48 relative">
+      <div className="object-cover w-80 md:w-96 h-48 relative">
         <Image
           src={imageUrl}
           layout="fill"
@@ -23,7 +23,7 @@ const NewsArticle: React.FC<NewsArticleProps> = ({ imageUrl, title, date }) => {
           objectFit="cover"
         />
       </div>
-      <div className="flex flex-col mt-4 w-96 h-20 relative">
+      <div className="flex flex-col mt-4 w-80 md:w-96 h-20 relative">
         <h3 className="text-base font-medium text-gray-300 line-clamp-2">
           {title}
         </h3>
